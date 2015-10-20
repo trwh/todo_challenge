3aac36e42074f11e0882ce29f3743c4232d9c158
nathanielgreen <ngreen2303@gmail.com>
2015-10-20 16:26:40 +0100

    Wired up the factory into our controller

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,15 +1,22 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource',function($resource) {
+githubUserSearch.controller('GitUserSearchController', ['Search',function(Search) {
 
   var self = this;
 
-  var searchResource = $resource('https://api.github.com/search/users');
+//  var searchResource = $resource('https://api.github.com/search/users');
 
   var access_token = gitAccessToken;
 
+//  self.doSearch = function() {
+//    self.searchResult = searchResource.get(
+//      { q: self.searchTerm, access_token:gitAccessToken } 
+//    );
+//  };
+
   self.doSearch = function() {
-    self.searchResult = searchResource.get(
-      { q: self.searchTerm, access_token:gitAccessToken } 
-    );
+    Search.query(self.searchTerm)
+      .then(function(response) {
+        self.searchResult = response.data;
+      })
   };
 
 }]);
