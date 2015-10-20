757148dad33066c3981b2a3ab7941efee86b6141
nathanielgreen <ngreen2303@gmail.com>
2015-10-20 10:55:24 +0100

    Added the ng-click to the index button, created the doSearch function, and modified the tests to include this function. All tests passing.

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -29,6 +29,8 @@ describe('GitUserSearchController', function() {
     ];
 
     it('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch();
       expect(ctrl.searchResult.items).toEqual(items);
     });
 
