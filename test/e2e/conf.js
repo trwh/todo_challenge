6f82dfebcb2107d3c80964ab99be61ddf51349b0
Tim Harrison <mail@trwh.co.uk>
2015-10-22 15:55:20 +0100

    In the middle of adding protractor mocks

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,4 +1,14 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['githubProfileFeature.js'],
+  mocks: {
+    default: ['mock-login'], // default value: []
+  }
+  onPrepare: function(){
+    require('protractor-http-mock').config = {
+        rootDirectory: __dirname, // default value: process.cwd()
+        protractorConfig: 'conf.conf' // default value: 'protractor.conf'
+    };
+  }
 }
+
--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,9 +1,12 @@
+var mock = require('protractor-http-mock');
+
 describe('GitHub profile finder', function() {
 
   var searchBox = element(by.model('searchCtrl.searchTerm'))
   var searchButton = element(by.className('btn'))
 
   beforeEach(function() {
+    mock(['githubUserSearch.js']);
     browser.get('http://localhost:8080');
   })
 
@@ -21,11 +24,11 @@ describe('GitHub profile finder', function() {
   });
 
   it('finds the last Spike', function() { //this is a silly test, but roll with it
-    searchBox.sendKeys('spike');
+    searchBox.sendKeys('spike01');
     searchButton.click();
 
     var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-    expect(profiles.last().getText()).toEqual('spikelynch');
+    expect(profiles.last().getText()).toEqual('spike01');
   });
 
 });
