2ba21a39a9972a15cbbcf77c13a661b4d5e42b5e
nathanielgreen <ngreen2303@gmail.com>
2015-10-22 11:59:08 +0100

    Completed step 4

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,9 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['githubProfileFeature.js'],
-  multiCapabilities: [{
-    browserName: 'firefox'
-  }, {
-    browserName: 'chrome'
-  }]
 }
--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -12,9 +12,20 @@ describe('GitHub profile finder', function() {
   });
 
   it('finds profiles', function() {
+
     searchBox.sendKeys('spike01');
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
     searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
+  });
+
+  it('finds the last Spike', function() { //this is a silly test, but roll with it
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spikelynch');
   });
 
 });
