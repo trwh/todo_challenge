086e897aff0af116800d9327a9651d01df7b1834
Tim Harrison <mail@trwh.co.uk>
2015-10-25 16:52:31 +0000

    Update controller spec.

--- test/toDoListController.spec.js
+++ test/toDoListController.spec.js
@@ -17,48 +17,4 @@ describe('GitUserSearchController', function() {
     expect(ctrl.toDoItems).toEqual(["Make TODO list"]);
   });
 
-  xdescribe('when seraching for a user', function() {
-
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
-
-    var httpBackend;
-
-    beforeEach(inject(function($httpBackend) {
-      httpBackend = $httpBackend
-      httpBackend
-      .expectGET("https://api.github.com/search/users?access_token=" + gitAccessToken + '&q=hello')
-      .respond(
-        { items: items }
-      );
-    }));
-
-    afterEach(function() {
-    httpBackend.verifyNoOutstandingExpectation();
-    httpBackend.verifyNoOutstandingRequest();
-   });
-
-    console.log(gitAccessToken)
-    it('displays search results', function() {
-      ctrl.searchTerm = 'hello';
-      ctrl.doSearch();
-      httpBackend.flush();
-      expect(ctrl.searchResult.items).toEqual(items);
-    });
-
-
-  });
-
-
-
 });
